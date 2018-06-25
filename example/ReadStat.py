d1568672a677d6968c63b6a9dbc0d3500ed68b0a
cangfengzhe <78956287@qq.com>
2018-06-25 09:03:14 +0800

    stat/ReadStat.py example

---
+++ example/ReadStat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+the example of stat/ReadStat class
+"""
+
+# ---------
+# Change Logs:
+#
+# ---------
+
+
+rs = ReadStat.ReadStat('data/test.fq.list',thread=4)
+reads_num = rs.get_reads_num() # get the number of reads
+reads_stat_dict = rs.length_stat()
+length_dist_N_dict, length_dist_over_dict = rs.length_distribute()
+rs.write_reads_stat('read_stat', # project name
+                    '~/test/', # output path
+                    reads_stat_dict,
+                    length_dist_N_dict,
+                    length_dist_over_dict)
